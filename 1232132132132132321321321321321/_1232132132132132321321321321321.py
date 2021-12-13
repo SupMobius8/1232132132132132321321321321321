d8e34083e381d6599479ae97f2ad63b0240a7ab1
Z1M8 <Z1M8@DESKTOP-02FBS0I>
2021-12-13 14:33:21 +0200

    Add project files.

---
+++ 1232132132132132321321321321321/_1232132132132132321321321321321.py
@@ -0,0 +1,42 @@
+﻿Userik=['Maks']
+Passik=['123']
+a = ''
+print('Hello user!')
+def newacc():
+    log=input('Write your login: ')
+    if log in Userik:
+        print('This Login exists.')
+        newacc()
+    else:
+        b=int(input('U want to create own pass or random? 1-own, 2-random: '))
+        if b==1:
+            passw= input('Create password: ')
+        if b==2:
+            import random
+            str0=".,:;!_*-+()/#¤%&"
+            str1 = '0123456789'
+            str2 = 'qwertyuiopasdfghjklzxcvbnm'
+            str3 = str2.upper()
+            str4 = str0+str1+str2+str3
+            ls = list(str4)
+            random.shuffle(ls)
+            passw = ''.join([random.choice(ls) for x in range(12)])
+            print('You are welcome',passw)
+        Userik.append(log)
+        Passik.append(passw)
+        print(f'Welcome back, {log}!')
+def oldacc():
+    login = input('Your login: ')
+    passw = input('Your password: ')
+    if login in Userik and passw in Passik:
+        print(f'Welcome back, {login}!')
+    else:
+        print('Incorect login or password, try one more time!')
+        oldacc()
+a=input('Are u our member? yes or no ? or if u want to quit, type: QUIT ===  ')
+if a=='Yes' or a=='yes':
+    oldacc()
+elif a=='No' or a=='no':
+    newacc()
+elif a=='QUIT':
+    quit()
